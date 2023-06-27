f2012c2b1226668a3e6908679b43b9b2a953b9e6
효진 <1004dlagywls@naver.com>
2023-06-27 11:32:24 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Order {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+}
