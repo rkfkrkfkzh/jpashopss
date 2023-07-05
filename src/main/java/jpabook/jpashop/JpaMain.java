3bede2ebf1294e493a5be9450f88bd12eeac3ce2
imhyojin <1004dlagywls@naver.com>
2023-07-06 02:53:22 +0900

    상속관계 매핑

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop;
 
 
+import jpabook.jpashop.domain.Book;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
@@ -9,6 +10,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
 
 public class JpaMain {
     public static void main(String[] args) {
@@ -21,13 +23,11 @@ public class JpaMain {
         tx.begin();
         try {
 
-            Order order = new Order();
-//            order.addOrderItem(new OrderItem());
-            em.persist(order);
-
-            OrderItem orderItem = new OrderItem();
-            orderItem.setOrder(order);
-            em.persist(orderItem);
+            Book book = new Book();
+            book.setAuthor("Lim");
+            book.setName("jpa");
+            book.setCreatedDate(LocalDateTime.now());
+            em.persist(book);
 
             tx.commit();
         } catch (Exception e) {
